dcd9626fe78c5afb4e953559c9bc9b323aa73617
Moritz Bürger <moritz.buerger@prodyna.com>
2017-12-18 21:41:43 +0100

    enhanced Session and added Game

---
+++ texting-server/src/main/java/de/marmor/texting/model/Game.java
@@ -0,0 +1,7 @@
+package de.marmor.texting.model;
+
+/**
+ * Created by mbuerger on 18.12.2017.
+ */
+public class Game {
+}
--- texting-server/src/main/java/de/marmor/texting/rest/Session.java
+++ texting-server/src/main/java/de/marmor/texting/rest/Session.java
@@ -1,7 +1,14 @@
 package de.marmor.texting.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by mbuerger on 16.12.2017.
@@ -9,8 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Session {
 
-	@RequestMapping("/")
-	public String test() {
-		return "Hello World!";
+	private static final Logger LOG = LoggerFactory.getLogger(Session.class);
+
+	private Set<String> companions = new HashSet<String>();
+
+	@RequestMapping(value = "/game/companion",  method = RequestMethod.GET)
+	public Set<String> get() {
+		return companions;
+	}
+
+	@RequestMapping(value = "/game/companion",  method = RequestMethod.PUT)
+	public String end(@RequestParam("name") String name) {
+		companions.add(name);
+		return String.format("Hallo, %s", name);
 	}
 }
