17678a39f7585d6e86dc470694c8036101c276a7
Moritz Bürger <moritz.buerger@prodyna.com>
2017-12-18 21:43:58 +0100

    Added name to Game

--- texting-server/src/main/java/de/marmor/texting/model/Game.java
+++ texting-server/src/main/java/de/marmor/texting/model/Game.java
@@ -4,4 +4,14 @@ package de.marmor.texting.model;
  * Created by mbuerger on 18.12.2017.
  */
 public class Game {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
