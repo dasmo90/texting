2d4ceaa19c8a367f87d1d00af92ae13275adf29c
Moritz Bürger <moritz.buerger@prodyna.com>
2017-12-27 17:09:29 +0100

    added start batch file

---
+++ texting-server/start.bat
@@ -0,0 +1,8 @@
+@echo off
+set ip_address_string="IPv4-Adresse"
+echo Network Connection Test
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do set ip_address=%%f
+set ip_address=%ip_address:~1%
+echo ^<html^>^<body^>Enter ^<p^>http://%ip_address%:8080^</p^> in your browser.^</body^>^</html^> > index.html
+start "" index.html
+java -jar texting-server-1.0-SNAPSHOT.jar
